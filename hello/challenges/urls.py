0f9ac0c013d7b2fb050ccd7206afb126555a9442
[vinald] <okiror1vinald@gmail.com>
2025-12-27 06:22:47 +0300

    moved to monthly branch

--- hello/challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.challenges, name='challenges'),
-    path('<int:month>/', views.challenge_by_number, name='challenge_by_number'),
-    path('<str:month>/', views.challenge, name='challenge')
-]
