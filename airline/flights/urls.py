cd10e38250f53006dcd36e4eb50d029693859c08
[vinald] <okiror1vinald@gmail.com>
2025-12-27 06:23:34 +0300

    moved projects to individal branches

--- airline/flights/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.flight_list, name='flight_list'),
-    path('<int:flight_id>/', views.flight_details, name='flight_details'),
-    path('<int:flight_id>/book/', views.book, name='book'),
-]
