addb1796a8800ea1cfde22751b23944b0475b271
[vinald] <okiror1vinald@gmail.com>
2026-01-10 09:29:27 +0300

    artiles

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_articles, name='article_home'),
+    path('<int:article_id>/', views.article_detail, name='article_detail'),
+    path('add/', views.add_article, name='add_article'),
 ]
