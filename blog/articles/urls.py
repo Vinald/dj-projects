3b07e9161d0227bafc6a639d5b4bfb1af6b5dbdf
Okiror Samuel Vinald <94779366+Vinald@users.noreply.github.com>
2026-01-10 09:55:18 +0300

    moved to monthly branch (#3)

--- blog/articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_articles, name='article_home'),
-    path('<int:article_id>/', views.article_detail, name='article_detail'),
-    path('add/', views.add_article, name='add_article'),
-]
