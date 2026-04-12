ea7c9d0acd7b4373b095bcfa448dcb95d8e6630e
[vinald] <okiror1vinald@gmail.com>
2026-04-13 02:19:09 +0300

    removed meetup

--- meetup/meetups/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('meetups/', views.MeetupListView.as_view(), name='meetup-list'),
-]
--- meetup/meetups/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Meetup
-
-# Create your views here.
-class MeetupListView(ListView):
-    model = Meetup
-    template_name = 'meetups/meetup_list.html'
-    context_object_name = 'meetups'
